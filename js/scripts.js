6cd733e6325a9f77859e4e3c2c19cd43a9fdc56f
Ayoub El Ouardi <112574618+ayoubelouardi@users.noreply.github.com>
2023-12-25 14:49:54 +0100

    add animation
    add animation of the span in the header

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,47 @@
         }
     });
 
+	/* Animation of the Full-name */
+	var words = ['Hi! I\'m Soufyane,', 'Graphic Guru', 'Color Captain', 'Color Captain', 'Pixel Wizard'],
+    part, i = 0, offset = 0, len = words.length, forwards = true, skip_count = 0, skip_delay = 15, speed = 70;
+	var wordflick = function () {
+	  setInterval(function () {
+		if (forwards) {
+		  if (offset >= words[i].length) {
+			++skip_count;
+			if (skip_count == skip_delay) {
+			  forwards = false;
+			  skip_count = 0;
+			}
+		  }
+		}
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+		$('.word').text(part);
+	  },speed);
+	};
 
+	$(document).ready(function () {
+	  wordflick();
+	});
+	
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
